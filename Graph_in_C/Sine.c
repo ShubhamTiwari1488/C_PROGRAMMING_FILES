bf496dc7efba143f8c0bb39b4cbf7073a7704bd5
shubham488-creator <73779227+shubham488-creator@users.noreply.github.com>
2021-06-17 18:04:11 +0530

    Add files via upload

---
+++ Graph_in_C/Sine.c
@@ -0,0 +1,41 @@
+
+/*________________________PLOT THE GRAPH OF sinx__________________________________________*/
+
+#include<stdio.h>
+#include<math.h>
+
+#define PI 3.142
+
+int main()
+{
+ //double y;
+ float x,rad;
+ int y,i,j;
+ 
+ //printf("\n%lf\n\n",sin(PI));
+ 
+ printf(" Y-------------------------------------------------------------------------------->\n");
+ 
+ for(i=0;i<=180;i+=15)
+ {
+  rad=(PI/180)*i;
+  
+  y=(int)(10*sin(rad));
+  y=fabs(y);
+  //printf("%lf\n",y); 
+  
+  if(i==90)
+     printf("X|");
+  else   
+     printf(" |");
+  
+  for(j=0;j<y;j++)
+  {
+   printf("*");
+  }
+  printf("#\n");
+   
+ }
+ 
+ return 0; 
+} 
