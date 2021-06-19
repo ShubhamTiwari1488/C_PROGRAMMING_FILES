6628b7f9ec82af2e6090d13eb0e140d6ee784544
shubham488-creator <73779227+shubham488-creator@users.noreply.github.com>
2021-06-20 00:02:09 +0530

    Update TheLeadGame.c

--- Codechef_Codes/TheLeadGame.c
+++ Codechef_Codes/TheLeadGame.c
@@ -1,4 +1,12 @@
- #include<stdio.h>
+/*____The Siruseri Sports Club organises an annual billiards game where the top two players of Siruseri play against each other.
+The Manager of Siruseri Sports Club decided to add his own twist to the game by changing the rules for determining the winner.
+In his version, at the end of each round, the cumulative score for each player is calculated, and the leader and her current lead are found.
+Once all the rounds are over the player who had the maximum lead at the end of any round in the game is declared the winner.
+Your task is to help the Manager find the winner and the winning lead. You may assume that the scores will be such that there will always be a single winner.
+That is, there are no ties.                          ___________________*/
+
+
+#include<stdio.h>
 
 int main()
 {
