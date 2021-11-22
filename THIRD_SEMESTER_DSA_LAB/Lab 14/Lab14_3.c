8eaaee2acb2333baba7e7376bf4dc9230d8e23af
shubham488-creator <73779227+shubham488-creator@users.noreply.github.com>
2021-11-22 11:06:44 +0530

    Add files via upload

---
+++ THIRD_SEMESTER_DSA_LAB/Lab 14/Lab14_3.c
@@ -0,0 +1,85 @@
+
+#include <stdio.h>
+#include<stdlib.h>
+
+struct Node 
+{
+	int data;
+	struct Node* left;
+	struct Node* right;
+};
+
+struct Node* newNode(int data)
+{
+	struct Node* node = (struct Node*)
+		malloc(sizeof(struct Node));
+	node->data = data;
+	node->left = NULL;
+	node->right = NULL;
+
+	return node;
+}
+
+void inorder(struct Node* root)
+{
+	if (root == NULL)
+		return;
+
+	inorder(root->left);
+
+	printf("%d ",root->data);
+
+	inorder(root->right);
+}
+
+
+int isIdentical(struct Node* root1,struct Node* root2)
+{
+	
+	if (root1 == NULL && root2 == NULL)
+		return 1;
+	
+	else if (root1 != NULL && root2 == NULL)
+		return 0;
+	else if (root1 == NULL && root2 != NULL)
+		return 0;
+	else 
+	{ 
+		if (root1->data == root2->data && isIdentical(root1->left, root2->left)
+			&& isIdentical(root1->right, root2->right))
+			return 1;
+		else
+			return 0;
+	}
+}
+
+int main()
+{
+	struct Node* root1 = newNode(5);
+	struct Node* root2 = newNode(5);
+	root1->left = newNode(3);
+	root1->right = newNode(8);
+	root1->left->left = newNode(2);
+	root1->left->right = newNode(4);
+	
+	printf("\n1st BST is :\t");
+	inorder(root1);
+	printf("\n");
+	
+	root2->left = newNode(3);
+	root2->right = newNode(8);
+	root2->left->left = newNode(2);
+	root2->left->right = newNode(4);
+	
+	printf("\n2nd BST is :\t");
+	inorder(root2);
+	printf("\n");
+	
+	if (isIdentical(root1, root2))
+		printf("\nBoth BSTs are identical\n");
+	else
+		printf("\nBSTs are not identical\n");
+
+	return 0;
+}
+
