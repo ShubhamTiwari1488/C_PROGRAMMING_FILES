ccea89a10217d699db756a88b073ffb0f8c751f5
SHUBHAM TIWARI <73779227+shubham488-creator@users.noreply.github.com>
2022-04-07 11:51:29 +0530

    Create BITWISE_SHIFT_OPERATION.c

---
+++ LoopsFunction_in_C/BITWISE_SHIFT_OPERATION.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int x=3;
+    
+    int y=40;
+    
+    printf("%d\n",x<<2); // Multiply by 2^2;
+    
+    printf("%d\n",y>>3); // Divide by 2^3;
+    
+    return 0;
+}
